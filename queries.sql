38c3914655f86cc391774a9b6f320c1b6a7d8355
Antimen777 <167418909+Antimen777@users.noreply.github.com>
2024-04-22 11:40:54 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,12 @@
 --Применяем агрегацию по id, чтобы посчитать количество покупателей.
 select count(customer_id) as customers_count
 from customers c
+
+--Соединяем нужные таблицы, считаем количество операций и общую выручку 10 лучших продавцов.
+select first_name || ' ' || last_name as seller, count(sales_person_id) as operations, floor(sum(quantity * price)) as income
+from employees e 
+join sales s on e.employee_id = s.sales_person_id
+join products p on s.product_id = p.product_id
+group by seller
+order by income desc
+limit 10
