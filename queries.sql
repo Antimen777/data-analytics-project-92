c3be5ce5f886fa24fcb81b2f44bc93ad2dbce3ec
Antimen777 <167418909+Antimen777@users.noreply.github.com>
2024-04-23 11:58:06 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,3 +27,13 @@ join sales s on e.employee_id = s.sales_person_id
 join products p on s.product_id = p.product_id
 group by seller, day_of_week, to_char(sale_date, 'ID')
 order by to_char(sale_date, 'ID'), seller
+
+--Через подзапрос группируем возраст на категории и считаем количество в каждой категории.
+with tab as (
+	select case when age between 16 and 25 then '16-25' when age between 26 and 40 then '26-40' else '40+' end as age_category
+	from customers
+)
+select age_category, count(age_category) as age_count
+from tab
+group by age_category
+order by age_category
